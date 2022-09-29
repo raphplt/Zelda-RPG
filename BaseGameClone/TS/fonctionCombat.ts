5cdecf7a377fd031f6db47dc3e9ccdb57013dbce
“raphael” <plassa_r@etna-alternance.net>
2022-09-29 09:00:01 -0400

    Test commit

--- BaseGameClone/TS/fonctionCombat.ts
+++ BaseGameClone/TS/fonctionCombat.ts
@@ -6,22 +6,21 @@ import { playerHP } from './main';
 
 export let death : boolean = false;
 
+function Heart(char : CharStats) {
+  let hearts : string = '';
+  for (let a : number = 0; a < char.hp; a += 1) {
+    hearts += '❤️';
+  }
+  console.log(`HP : ${hearts}`);
+}
+
 export function combat(enemy : CharStats, hero: CharStats) {
   const maxHP : number = playerHP;
   const halfMaxHp : number = (maxHP / 2);
   console.log(`\x1b[0;31m${enemy.name}\x1b[0m has \x1b[0;31m${enemy.hp}\x1b[0m hp.`);
-  let EnemyHearts : string = '';
-  for (let a : number = 0; a < (enemy.hp / 10); a += 1) {
-    EnemyHearts += '❤️';
-  }
-  console.log(`HP : ${EnemyHearts}`);
-
+  Heart(enemy);
   console.log(`\x1b[0;32mYou\x1b[0m have \x1b[0;32m${hero.hp}\x1b[0m hp.`);
-  let HeroHearts : string = '';
-  for (let a : number = 0; a < (hero.hp / 10); a += 1) {
-    HeroHearts += '❤️';
-  }
-  console.log(`HP : ${HeroHearts}`);
+  Heart(hero);
   let enemyHp : number = enemy.hp;
 
   while (enemyHp > 0) {
@@ -35,9 +34,15 @@ export function combat(enemy : CharStats, hero: CharStats) {
         enemyHp = 0;
       }
       console.log(`\x1b[0;31m${enemy.name}\x1b[0m has now \x1b[0;31m${enemyHp}\x1b[0m hp.`);
+      Heart(enemy);
       if (enemyHp > 0) {
-        hero.hp -= enemy.str;
+        if (hero.str < enemy.hp) {
+          hero.hp -= enemy.str;
+        } else {
+          hero.hp = 0;
+        }
         console.log(`\x1b[0;31m${enemy.name}\x1b[0m attacks you, \x1b[0;32myou\x1b[0m have \x1b[0;32m${hero.hp}\x1b[0m hp remaining.`);
+        Heart(hero);
       }
       if (hero.hp <= 0) {
         console.log('You die. Try again');
@@ -52,12 +57,14 @@ export function combat(enemy : CharStats, hero: CharStats) {
         hero.hp -= enemy.str;
         console.log(`You have restored yourself ${Math.floor(halfMaxHp)}hp`);
         console.log(`You have curently ${hero.hp} hp.`);
+        Heart(hero);
       } else {
         hero.hp = maxHP;
         hero.hp -= enemy.str;
         console.log('You have restored all your HP.');
       }
       console.log(`\x1b[0;31m${enemy.name}\x1b[0m attacks you, \x1b[0;32myou\x1b[0m have \x1b[0;32m${hero.hp}\x1b[0m hp remaining.`);
+      Heart(hero);
       if (hero.hp <= 0) {
         console.log('You die. Try again');
         death = true;
@@ -70,7 +77,8 @@ export function combat(enemy : CharStats, hero: CharStats) {
     }
     if (action === 3) {
       hero.hp -= enemy.str;
-      console.log(`\x1b[0;31m${enemy.name}\x1b[0m attacks you, but you only suffer half the damage \x1b[0;32myou\x1b[0m have \x1b[0;32m${hero.hp}\x1b[0m hp remaining.`);
+      console.log(`\x1b[0;31m${enemy.name}\x1b[0m attacks you, but you only suffer half the damage, \x1b[0;32myou\x1b[0m have \x1b[0;32m${hero.hp}\x1b[0m hp remaining.`);
+      Heart(hero);
       if (hero.hp <= 0) {
         console.log('You die. Try again');
         death = true;
